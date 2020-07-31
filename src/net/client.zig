f89cc8de0ea6071b88c38a70dcad0bfc55eb6630
Maurice Schmidt <mvmogo@gmail.com>
2020-07-31 10:49:57 +0200

    Improve client handling (kind of)

--- src/net/client.zig
+++ src/net/client.zig
@@ -1,17 +1,18 @@
 const std = @import("std");
 const Connection = std.net.StreamServer.Connection;
 const TcpServer = @import("server.zig").TcpServer;
+const ArrayList = std.ArrayList;
 
 pub const Client = struct {
     context: *const Connection,
     handle_frame: @Frame(handle),
 
     fn handle(self: *Client, server: *TcpServer) !void {
-        var buf: [_]u8 = undefined;
-        const amt = try self.context.file.read(&buf);
-        const msg = buf[0 .. amt];
+        var buffer: [comptime 1024 * 2]u8 = undefined;
+        const bytes_read = try self.context.file.read(&buffer);
+        const message = buffer[0 .. bytes_read];
 
         for (server.message_handlers.items) |handler| 
-            handler(self, msg);
+            handler(self, message);
     }
 };
--- src/webzig.zig
+++ src/webzig.zig
@@ -7,5 +7,7 @@ pub fn main() !void {
     const allocator = std.heap.page_allocator;
     
     var server = http.server.HttpServer.init(allocator);
+    defer server.deinit();
+    
     try server.listen();
 }
