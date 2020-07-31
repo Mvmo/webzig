f386ccf2203ff9997c29d5f8f71f5f09d9a2caed
Maurice Schmidt <mvmogo@gmail.com>
2020-07-31 05:41:24 +0200

    Add basic working server implementation

--- src/server.zig
+++ src/server.zig
@@ -1,33 +1,60 @@
 const std = @import("std");
 const net = std.net;
+const mem = std.mem;
 
-const Server = struct {
+const Connection = net.StreamServer.Connection;
+
+const Client = struct {
     
-    address: net.Address,
-    streamServer: net.StreamServer,
+    context: *const Connection,
+    handle_frame: @Frame(handle),
 
-    pub fn init() Server {
-        return Server {
-            .address = net.Address.parseIp4("127.0.0.1", 1889) catch unreachable,
-            .streamServer = net.StreamServer.init(net.StreamServer.Options{}),
-        };
-    }
-
-    fn listen(self: *Server) !void {
-        try self.streamServer.listen(self.address);
-    }
-
-    fn helloWorld(self: *Server) void {
+    fn handle(self: *Client, server: *Server) !void {
+        _ = try self.context.file.write("server: welcome to the server");
     }
 
 };
 
-const assert = std.debug.assert;
+const Server = struct {
 
-test "create server" {
-    var server = Server.init();
-    
+    allocator: *mem.Allocator,
+    address: net.Address,
+    stream_server: net.StreamServer,
+
+    pub fn init(allocator: *mem.Allocator) Server {
+        return Server {
+            .allocator = allocator,
+            .address = net.Address.parseIp4("127.0.0.1", 1889) catch unreachable,
+            .stream_server = net.StreamServer.init(net.StreamServer.Options{}),
+        };
+    }
+
+    fn deinit(self: *Server) void {
+        self.* = undefined;
+    }
+
+    fn listen(self: *Server) !void {
+        try self.stream_server.listen(self.address);
+        while (true) {
+            const connection = try self.stream_server.accept();
+            const client = try self.allocator.create(Client);
+
+            client.* = .{
+                .context = &connection,
+                .handle_frame = async client.handle(self),
+            };            
+        }
+    }
+
+};
+
+test "create server and listen" {
+    const allocator = std.heap.page_allocator;
+    var server = Server.init(allocator);
+
+    defer server.deinit();
+
     std.debug.warn("Listening on Port {}\n", .{ server.address.getPort() });
 
-    _ = server.helloWorld();
+    _ = try server.listen();
 }
