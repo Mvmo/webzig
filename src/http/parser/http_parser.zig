c20da76a068d64599ad7f85309be249ccd41338a
Maurice Schmidt <mvmogo@gmail.com>
2020-08-03 04:00:00 +0200

    Remove request.zig

--- src/http/parser/http_parser.zig
+++ src/http/parser/http_parser.zig
@@ -1,1 +1,21 @@
 const Request = struct {};
+
+const RequestParser = struct {
+
+    var cursor: u32;
+
+    original_input: *[]const u8,
+    consumed_input: *[]const u8,
+    request_type: []const u8,
+
+    pub fn parse(input: *[]const u8) RequestParser {
+        return RequestParser {
+            .original_input = original_input,
+            .consumed_input = "",
+            .request_type = ""
+        };
+    }
+
+};
+
+const std = @import("std");
