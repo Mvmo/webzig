5d304e18bbf5d13ff46b4bbfd717a82df81ad3d8
Maurice Schmidt <mvmogo@gmail.com>
2020-08-03 07:57:13 +0200

    Create Response Struct

--- src/http/http.zig
+++ src/http/http.zig
@@ -1,3 +1,5 @@
 pub const server = @import("http_server.zig");
 pub const request = @import("request.zig");
 pub const header = @import("header.zig");
+
+pub const Response = @import("response.zig").Response;
---
+++ src/http/response.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+pub const Response = struct {
+    const version: []const u8 = "HTTP/1.1";
+
+    status_code: u8,
+    status_message: *[]const u8,
+
+    body: *[]const u8,
+
+    pub fn asString(self: *Response) ![]const u8 {
+        return try std.fmt.allocPrint(std.heap.page_allocator, "{} {} {}\r\n{}\r\n{}{}\r\n\r\n{}", .{
+            Response.version,
+            self.status_code,
+            self.status_message.*,
+            "Content-Type: text/html",
+            "Content-Length: ",
+            self.body.len,
+            self.body.*,
+        });
+    }
+};
