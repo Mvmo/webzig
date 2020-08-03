5d1286e0e143655da6d77ed795568341028d58e9
Maurice Schmidt <mvmogo@gmail.com>
2020-08-03 02:40:05 +0200

    Remove no-sense code and import tests again

--- src/tests.zig
+++ src/tests.zig
@@ -1,10 +1,7 @@
 const std = @import("std");
 
 test "test the application" {
-    //_ = @import("webzig.zig");
-    //_ = @import("net/net.zig");
-    //_ = @import("http/http.zig");
-    var str: []const u8 = "Hello, World!";
-    std.debug.warn("{}\n", .{@typeName(@TypeOf(&str))});
-
+    _ = @import("webzig.zig");
+    _ = @import("net/net.zig");
+    _ = @import("http/http.zig");
 }
