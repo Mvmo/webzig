04046f038f60a645e4179e9885736b5ec7becc02
Maurice Schmidt <mvmogo@gmail.com>
2020-08-03 12:28:53 +0200

    Done header parsing

--- src/http/parser/header_parser.zig
+++ src/http/parser/header_parser.zig
@@ -1,1 +1,22 @@
-// TODO interact with headers later
+const std = @import("std");
+
+const parser = @import("parser.zig");
+
+pub fn parse(input: []const u8) std.http.Headers {
+    var headers = std.http.Headers.init(std.heap.page_allocator);
+
+    var cursor: u64 = 0;
+    while (parser.readLine(input[cursor..])) |line| : (cursor += line.len + 2) {
+        if (line.len == 0)
+            break;
+
+        var key = parser.readToken(input[cursor..]) catch unreachable;
+        key = key[0..key.len - 1];
+
+        var value = parser.readToken(input[cursor + key.len + 2..]) catch unreachable;
+
+        _ = headers.append(key, value, null) catch unreachable;
+    }
+
+    return headers;
+}
--- src/http/parser/request_parser.zig
+++ src/http/parser/request_parser.zig
@@ -6,18 +6,22 @@ const readToken = parser.readToken;
 
 const String = []const u8;
 
+const header = @import("header_parser.zig");
+
 pub const Request = struct {
     request_line: String,
     method: String,
     uri: String,
     version: String,
+    headers: std.http.Headers,
 
     pub fn print(self: *Request) void {
-        std.debug.warn("\nRequest-Line: {}\n\nMethod: {}\nUri: {}\nVersion: {}\n", .{
+        std.debug.warn("\nRequest-Line: {}\n\nMethod: {}\nUri: {}\nVersion: {}\nHeaders: {}\n", .{
             self.request_line,
             self.method,
             self.uri,
             self.version,
+            self.headers.toSlice().len
         });
     }
 };
@@ -27,11 +31,13 @@ pub fn parse(input: String) !Request {
     const method = try readToken(request_line);
     const uri = try readToken(request_line[method.len + 1 ..]);
     const version = try readToken(request_line[method.len + uri.len + 2 ..]);
+    const headers = header.parse(input[request_line.len + 2..]);
 
     return Request{
         .request_line = request_line,
         .method = method,
         .uri = uri,
         .version = version,
+        .headers = headers,
     };
 }
