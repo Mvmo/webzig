79636834800a35c8fed229055e2b0b6150fe06ad
Maurice Schmidt <mvmogo@gmail.com>
2020-08-03 05:36:19 +0200

    Remove tests

--- src/http/http.zig
+++ src/http/http.zig
@@ -1,7 +1,3 @@
 pub const server = @import("http_server.zig");
 pub const request = @import("request.zig");
 pub const header = @import("header.zig");
-
-test "run http tests" {
-    _ = @import("parser/http_parser.zig");
-}
